73b0c9782d28d3f13558ef1a37675d05270b848d
smbock42 <smbock42@gmail.com>
2023-10-30 11:32:11 -0700

    updated json uploading

--- json_to_db.py
+++ json_to_db.py
@@ -16,20 +16,28 @@ def main():
     with open(path, "r") as f:
         data = load(f)
     with db.engine.begin() as connection:
-        schema = [key for key in data[0].keys()]
-        sql = f"INSERT into users (name, address, phone, email) VALUES (:name, :address, :phone, :email)"
-        parameters = {
-            "name":"Test",
-            "address":"Test",
-            "phone":"Test",
-            "email":"Test"
+        part_type = filename.split(".")[0]
+        schemas = [key for key in data[0].keys()]
+        for value in data:
+            sql = f"INSERT into part_inventory (name, type, quantity) VALUES (:name, :type, :quantity) RETURNING part_id"
+            parameters = {
+                "name":str(value["name"]),
+                "type":str(part_type),
+                "quantity":0
+            }
+            result = connection.execute(statement=sqlalchemy.text(sql),parameters=parameters)
+            part_id = result.first()[0]
+            sql = f"INSERT into {part_type}_specs (part_id, {', '.join(schemas)}) VALUES (:part_id,{', '.join([f':{column_name}' for column_name in schemas])})"
+            parameters = {
+                "part_id": part_id,
+            }
+            for schema in schemas:
+                parameters[schema] = value[schema]
             
-        }
-        result = connection.execute(statement=sqlalchemy.text(sql),parameters=parameters)
-        
+            result = connection.execute(statement=sqlalchemy.text(sql),parameters=parameters)
 
-    ### INSERT to inventory table return ID and put inventory id in insert table
-    
+        ### INSERT to inventory table return ID and put inventory id in insert table
+        
 
 
 if __name__ == "__main__":
