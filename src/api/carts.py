67a88ff6a3ac4a89594da21f5c8bc8b37d9edf79
nckptrck <nckptrck1@gmal.com>
2023-10-30 11:25:57 -0700

    base for carts

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,1 +1,48 @@
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
 
+router = APIRouter(
+    prefix="/carts",
+    tags=["cart"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+
+
+class NewCart(BaseModel):
+    customer: str
+
+
+@router.post("/")
+def create_cart(new_cart: NewCart):
+    """ """
+    return {"cart_id": 1}
+
+
+@router.get("/{cart_id}")
+def get_cart(cart_id: int):
+    """ """
+
+    return {}
+
+
+class CartItem(BaseModel):
+    quantity: int
+
+
+@router.post("/{cart_id}/items/{item_sku}")
+def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
+    """ """
+
+    return "OK"
+
+
+class CartCheckout(BaseModel):
+    payment: str
+
+@router.post("/{cart_id}/checkout")
+def checkout(cart_id: int, cart_checkout: CartCheckout):
+    """ """
+
+    return {"total_potions_bought": 1, "total_gold_paid": 50}
