57d9d8681e14f836258bfebec988d373f7340098
AdamDelRio <adamdelrio8523@gmail.com>
2023-11-03 11:13:46 -0700

    login code

---
+++ src/api/login.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends
+from src.api import auth
+from sqlalchemy.exc import IntegrityError
+from src import database as db
+import sqlalchemy
+router = APIRouter(
+    prefix="/login",
+    tags=["login"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+@router.post("/signup/")
+def sign_up(username: str, email: str):
+    """
+    Sign up for the site.
+    """
+    try:
+        with db.engine.begin() as connection:
+            connection.execute(
+                sqlalchemy.text(
+                    "INSERT INTO users (username, email) VALUES (:username, :email) RETURNING id"
+                ),
+                {"username": username, "email": email}
+            )
+        return {"message": "User signed up successfully"}
+    except IntegrityError as e:
+        db.engine.rollback()
+        return {"error": "Username or email already taken"}
+    
+
+@router.post("/login/")
+def login(username: str):
+    """
+    Log in for the site.
+    """
+    with db.engine.begin() as connection:
+        result = connection.execute(
+            sqlalchemy.text(
+                "SELECT * FROM users WHERE username = :username RETURNING id"
+            ),
+            {"username": username}
+        ).fetchone()
+
+        if result is None:
+            return {"message": "Invalid username"}
+        else:
+            return {"message": "Login successful"}
