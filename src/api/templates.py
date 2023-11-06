5811d51a61b3f6fadb11f40375ac89dc425cac8e
nckptrck <nckptrck1@gmal.com>
2023-11-06 12:40:20 -0800

    templates working

--- src/api/templates.py
+++ src/api/templates.py
@@ -43,19 +43,45 @@ def add_item_to_template(user_id, template_id, part_id, template_part: TemplateP
                                            })
     return "OK"
         
+class NewCart(BaseModel):
+    user_id: int
+    name: str
+    address:str
+    phone:str
+    email:str
+
 
 @router.post('/{template_id}/cart/new')
-def create_cart_from_template(template_id):
+def create_cart_from_template(template_id, new_cart:NewCart):
     with db.engine.begin() as connection:
+        cust_id = connection.execute(
+            sqlalchemy.text("SELECT id FROM customers WHERE "
+                            "name = :name AND address = :address AND "
+                            "phone = :phone AND email = :email")
+            .params(name=new_cart.name, address=new_cart.address, 
+                    phone=new_cart.phone, email=new_cart.email)
+        ).scalar()
+
+        if not cust_id:
+            cust_id = connection.execute(sqlalchemy.text("INSERT INTO customers (user_id, name, address, phone, email) "
+                                            "VALUES (:user_id, :name, :address, :phone, :email) "
+                                            "RETURNING id "), parameters = dict(user_id = new_cart.user_id,
+                                                                                name = new_cart.name,
+                                                                                address = new_cart.address,
+                                                                                phone = new_cart.phone,
+                                                                                email = new_cart.email)).scalar()
+        
         cart_id = connection.execute(sqlalchemy.text("INSERT INTO carts (user_id) "
-                                                     "SELECT pc_templates.user_id "
+                                                     "SELECT customers.id "
                                                      "FROM pc_templates "
+                                                     "JOIN customers ON customers.user_id = pc_templates.user_id "
                                                      "WHERE pc_templates.id = :template_id "
                                                      "RETURNING cart_id"),
                                                      parameters= dict(template_id = template_id)).scalar()
-        connection.execute(sqlalchemy.text("INSERT INTO cart_items (cart_id, part_id, quantity, user_part)"
-                                           "SELECT :cart_id, pc_template_parts.part_id, pc_template_parts.quantity, pc_template_part.user_part "
+        connection.execute(sqlalchemy.text("INSERT INTO cart_items (cart_id, part_id, quantity, user_item)"
+                                           "SELECT :cart_id, pc_template_parts.part_id, pc_template_parts.quantity, pc_template_parts.user_part "
                                            "FROM pc_template_parts "
-                                           "WHERE pc_template_parts.template_id = :template_id "),\
+                                           "WHERE pc_template_parts.template_id = :template_id "),
                                             parameters= dict(cart_id = cart_id,
                                                              template_id = template_id))
+        return {"cart_id": cart_id}
