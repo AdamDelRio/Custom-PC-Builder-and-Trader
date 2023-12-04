b1fcd7c4698c1eb06e580a363a094030432c5e7c
smbock42 <smbock42@gmail.com>
2023-12-03 19:50:48 -0800

    added randomize_quantity and add_users

--- populate_posts.py
+++ populate_posts.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def database_connection_url():
-    dotenv.load_dotenv()
+    dotenv.load_dotenv('passwords.env')
     DB_USER: str = os.environ.get("POSTGRES_USER")
     DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
     DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
@@ -14,32 +14,53 @@ def database_connection_url():
     DB_NAME: str = os.environ.get("POSTGRES_DB")
     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
-# Create a new DB engine based on our connection string
-engine = sqlalchemy.create_engine(database_connection_url(), use_insertmanyvalues=True)
 
-num_users = 200000
-fake = Faker()
-posts_sample_distribution = np.random.default_rng().negative_binomial(0.04, 0.01, num_users)
-category_sample_distribution = np.random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                                 num_users,
-                                                p=[0.1, 0.05, 0.1, 0.3, 0.05, 0.05, 0.05, 0.05, 0.15, 0.1])
-total_posts = 0
+def randomize_quantity():
+    engine = sqlalchemy.create_engine(database_connection_url(), use_insertmanyvalues=True)
 
-# create fake posters with fake names and birthdays
-with engine.begin() as conn:
-    print("creating fake posters...")
-    posts = []
-    for i in range(num_users):
-        if (i % 10 == 0):
-            print(i)
-        
+    #iterate through all items in part_inventory and update quantity to a random number between 0 and 100
+    with engine.begin() as conn:
+        parts = conn.execute(sqlalchemy.text("""
+        SELECT * FROM part_inventory;
+        """)).fetchall()
 
-        email = fake.unique.email()
-        username = email.split('@')[0]
+        for part in parts:
+            quantity = np.random.randint(0, 100)
+            conn.execute(sqlalchemy.text("""
+            UPDATE part_inventory SET quantity = :quantity WHERE part_id = :part_id;
+            """), {"quantity": quantity, "part_id": part[0]})
 
-        poster_id = conn.execute(sqlalchemy.text("""
-        INSERT INTO users (username, email) VALUES (:username, :email) RETURNING id;
-        """), {"username": username, "email": email, }).fetchone()[0]
+def add_users(num_users):
+    # Create a new DB engine based on our connection string
+    engine = sqlalchemy.create_engine(database_connection_url(), use_insertmanyvalues=True)
 
-    print("total posts: ", total_posts)
-    
+    fake = Faker()
+
+    # create fake posters with fake names and birthdays
+    with engine.begin() as conn:
+        posts = []
+        for i in range(num_users):
+            if (i % 10 == 0):
+                print(i)
+            
+
+            email = fake.unique.email()
+            username = email.split('@')[0]
+            # check if username exists. Return bool true/false
+            while True:
+                username_exists = conn.execute(sqlalchemy.text("""
+                SELECT * FROM users WHERE username = :username;
+                """), {"username": username}).fetchone()
+                if username_exists:
+                    email = fake.unique.email()
+                    username = email.split('@')[0]
+                else:
+                    break
+
+            # if username exists, skip this iteration of the loop
+            conn.execute(sqlalchemy.text("""
+            INSERT INTO users (username, email) VALUES (:username, :email);
+            """), {"username": username, "email": email, })
+
+if __name__ == "__main__":
+    randomize_quantity()
